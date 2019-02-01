27cff699f02ffb2230511ccf68fce6c9f241ad3d
LongYinan <lynweklm@gmail.com>
2019-02-01 11:56:46 +0800

    fix css compile

--- tools/postcss.config.js
+++ tools/postcss.config.js
@@ -1,21 +1,21 @@
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
+const postcssPresetEnv = require('postcss-preset-env')
 const { browserslist } = require('../package')
 
-module.exports = function(context) {
+module.exports = function() {
   const plugins = [
+    postcssPresetEnv({
+      stage: 0,
+    }),
     autoprefixer({
       browsers: browserslist,
     }),
+    require('postcss-nested'),
+    cssnano(),
   ]
 
-  if (process.env.NODE_ENV === 'production') {
-    plugins.push(cssnano())
-  }
-
   return {
-    from: context.from,
     plugins,
-    to: context.to,
   }
 }
